25c7c876e15e0733a93cb51dc97b2d5149c77073
Kunal Varma <kunalvarma05@gmail.com>
2016-02-14 04:45:32 +0530

    TheMovieDB Common Collection Class

---
+++ app/src/main/java/in/kunalvarma/chillflix/TheMovieDB/Common/Collection.java
@@ -0,0 +1,33 @@
+package in.kunalvarma.chillflix.TheMovieDB.Common;
+
+public class Collection {
+
+
+    private int page;
+    private int totalResults;
+    private int totalPages;
+
+    /**
+     * @return The page
+     */
+    public int getPage() {
+        return page;
+    }
+
+
+    /**
+     * @return The totalResults
+     */
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+
+    /**
+     * @return The totalPages
+     */
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+}
