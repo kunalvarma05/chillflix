ba17e214f787496f648922e10ce502c9cc2f48ad
Kunal Varma <kunalvarma05@gmail.com>
2016-02-14 04:49:57 +0530

    TheMovieDB Discover Model

---
+++ app/src/main/java/in/kunalvarma/chillflix/TheMovieDB/Model/Discover.java
@@ -0,0 +1,11 @@
+package in.kunalvarma.chillflix.TheMovieDB.Model;
+
+import in.kunalvarma.chillflix.TheMovieDB.Common.MovieCollection;
+
+public class Discover extends MovieCollection {
+
+    public Discover() {
+        super();
+    }
+
+}
