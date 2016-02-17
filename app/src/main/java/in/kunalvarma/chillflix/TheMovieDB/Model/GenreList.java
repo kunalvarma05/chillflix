92ee1e47255fc3d72e8b71251abf134c1af93a27
Kunal Varma <kunalvarma05@gmail.com>
2016-02-18 01:42:48 +0530

    GenreList Model

---
+++ app/src/main/java/in/kunalvarma/chillflix/TheMovieDB/Model/GenreList.java
@@ -0,0 +1,14 @@
+package in.kunalvarma.chillflix.TheMovieDB.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreList {
+
+    public List<Genre> genres = new ArrayList<Genre>();
+
+    public List<Genre> getGenres() {
+        return this.genres;
+    }
+
+}
